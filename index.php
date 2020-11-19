51c46172d35f9f0495c29af044ae70e4ef551e35
Nikollas Victor Beltrão de Santana <64939921+NikollasBeltrao@users.noreply.github.com>
2020-11-18 22:13:39 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, FILES");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
+?>
