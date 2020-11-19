09bf81ee80f34cb3ac28801aa4c6ea7a0326ad03
Nikollas Victor Beltrão de Santana <64939921+NikollasBeltrao@users.noreply.github.com>
2020-11-18 22:33:22 -0300

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, FILES");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-
-?>
